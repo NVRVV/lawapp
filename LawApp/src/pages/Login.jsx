8b12e439cb4718c88d52d4b1185c387f1330d14e
NVRVV <varmanamepalli003@gmail.com>
2025-02-21 00:37:13 +0530

    updation

---
+++ LawApp/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+      <section className='bg-gray-800 h-screen text-white'>
+            <h1>jkajdjfkalfdjsaljf</h1>
+      </section>
+    </>
+  )
+}
+
+export default Login
