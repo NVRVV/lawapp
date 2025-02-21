3d69ae173faed015e2b211df6f628fbd99ea1cd2
NVRVV <varmanamepalli003@gmail.com>
2025-02-21 18:48:16 +0530

    adfasdf

--- LawApp/src/App.jsx
+++ LawApp/src/App.jsx
@@ -3,12 +3,14 @@ import Login from './pages/Login'
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from './pages/Register';
+import Header from './components/Header';
 
 const App = () => {
   return (
     
     <Router>
       <div>
+        <Header/>
         <Routes>
         <Route path='/login' element={<Login />}></Route>
         <Route path='/register' element={<Register/>}></Route>
