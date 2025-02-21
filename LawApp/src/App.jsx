241628bf53043429e9f7c38d773fcefb38a235ea
NVRVV <varmanamepalli003@gmail.com>
2025-02-21 19:18:18 +0530

    updates

--- LawApp/src/App.jsx
+++ LawApp/src/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import Login from './pages/Login'
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from './pages/Register';
-
-import Header from './components/Header';
-
 import Home from './pages/Home';
+import Header1 from './components/Header1';
 
 
 const App = () => {
@@ -14,11 +11,10 @@ const App = () => {
     
     <Router>
       <div>
-        <Header/>
         <Routes>
         <Route path='/login' element={<Login />}></Route>
         <Route path='/register' element={<Register/>}></Route>
-        <Route path='/' element={<Home/>}></Route>
+        <Route path='/' element={<><Header1/><Home/></>}></Route>
         </Routes>
         
       </div>
--- LawApp/src/pages/Home.jsx
+++ LawApp/src/pages/Home.jsx
@@ -3,10 +3,14 @@ import '../index.css';
 import hiw1 from '../assets/hiw-1.png';
 import hiw2 from '../assets/hiw-2.png';
 import hiw3 from '../assets/hiw-3.png';
+import hero from '../assets/hero.png'
 
 const Home = () => {
   return (
-    <section className='bg-bg2 h-screen flex flex-col justify-center'>
+    <section className='bg-bg2 flex flex-col justify-center'> 
+        <div>
+            <img src={hero} alt="" />
+        </div>
       <div className="flex flex-row md:flex-col items-center justify-center mb-10">
         <h1 className='text-6xl'>How it works</h1>        
       </div>
