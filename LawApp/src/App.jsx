c4cc3aaabca899937e83f0d058934b21905a2b49
AnuhyaPaleti <anuhyapaleti@gmail.com>
2025-02-21 18:44:30 +0530

    Home page

--- LawApp/src/App.jsx
+++ LawApp/src/App.jsx
@@ -3,6 +3,7 @@ import Login from './pages/Login'
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from './pages/Register';
+import Home from './pages/Home';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Routes>
         <Route path='/login' element={<Login />}></Route>
         <Route path='/register' element={<Register/>}></Route>
+        <Route path='/' element={<Home/>}></Route>
         </Routes>
         
       </div>
--- LawApp/src/pages/Home.jsx
+++ LawApp/src/pages/Home.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from 'react';
+import '../index.css';
+import hiw1 from '../assets/hiw-1.png';
+import hiw2 from '../assets/hiw-2.png';
+import hiw3 from '../assets/hiw-3.png';
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    <section className='bg-bg2 h-screen flex flex-col justify-center'>
+      <div className="flex flex-row md:flex-col items-center justify-center mb-10">
+        <h1 className='text-6xl'>How it works</h1>        
+      </div>
+
+      {/* Parent div with gap & equal height distribution */}
+      <div className="flex flex-col md:flex-row items-center justify-center gap-30  ml-20 mr-20 pb-20">
+        <div className="flex flex-col flex-1 bg-bg w-full h-120 items-center justify-center p-2 rounded-lg shadow-lg">
+          <img src={hiw1} alt="Client Submission" />
+          <h1 className='text-3xl pt-10 pb-15 text-center'>Client Case Submission</h1>
+          <p className='px-5 pb-5 text-center'>
+            Clients register on the platform and provide essential details about their legal case.
+            The system collects case-related information such as case type, legal issue, location, and urgency.
+          </p>
+        </div>
+
+        <div className="flex flex-col flex-1 bg-bg w-full h-120 items-center justify-center p-2 rounded-lg shadow-lg">
+          <img src={hiw2} alt="Case Review" className='pt-8'/>
+          <h1 className='text-3xl pt-8 pb-5 text-center'>Lawyer Matching and Recommendation</h1>
+          <p className='px-5 pb-5 text-center'>
+          The platform employs an advanced matching algorithm to recommend lawyers based on their legal expertise, availability, and proximity for in-person consultations.
+          Clients receive a list of recommended lawyers
+          </p>
+        </div>
+
+        <div className="flex flex-col flex-1 bg-bg w-full h-120 items-center justify-center p-2 rounded-lg shadow-lg ">
+          <img src={hiw3} alt="Legal Consultation" className='pt-7'/>
+          <h1 className='text-3xl pt-5 pb-5 p-2 text-center'>Real-Time Communication & Updates</h1>
+          <p className='px-5 pb-5 text-center'>
+          Both clients and lawyers receive notifications for status updates, reminders, and confirmations.
+          The platform can integrate messaging or video conferencing for virtual consultations.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col md:flex-row bg-bg items-center justify-center">
+        <div className="flex ">
+        <h1 className='text-7xl'>Schedule a free case evolution</h1>
+        </div>
+        <div className="flex">
+            <p>The platform leverages an intelligent matching algorithm to seamlessly connect clients with the most suitable lawyers based on expertise, availability, and location, ensuring an efficient and streamlined legal consultation process.</p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Home
+export default Home;
