5a871e3114f1e262c24e4911ae58bfeb1e2c1fa5
AnuhyaPaleti <anuhyapaleti@gmail.com>
2025-02-22 18:36:48 +0530

    update

--- LawApp/src/App.jsx
+++ LawApp/src/App.jsx
@@ -3,14 +3,11 @@ import Login from './pages/Login'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from './pages/Register';
 import Home from './pages/Home';
-<<<<<<< HEAD
-=======
+
 
 import Filter from './pages/filter/Filter';
 import LawyerForm from './pages/LawyerForm';
 
-
->>>>>>> 7cf7dcb81bcbf33680f5e18699a9e33aa35f3c96
 import Header1 from './components/Header1';
 import FormScreen from './pages/Form/FormScreen';
 import UploadScreen from './pages/Form/UploadScreen';
@@ -24,25 +21,17 @@ const App = () => {
       <div>
         <Routes>
         <Route path='/login' element={<Login />}></Route>
-<<<<<<< HEAD
         <Route path='/register' element={<Register/>}></Route>
-=======
-        <Route path='/register' element={<Register/>}></Route>     
-
->>>>>>> 7cf7dcb81bcbf33680f5e18699a9e33aa35f3c96
+        <Route path='/register' element={<Register/>}></Route>   
         <Route path='/' element={<><Header1/><Home/></>}></Route>
         <Route path='/form' element={<FormScreen/>}></Route>
         <Route path='/upload' element={<UploadScreen setUploadedFiles={setUploadedFiles} uploadedFiles={uploadedFiles}/>}></Route>
         <Route path='/review' element={<ReviewScreen uploadedFiles={uploadedFiles} setUploadedFiles={setUploadedFiles}/>}></Route>
 
         <Route path='/' element={<><Home/></>}></Route>
-<<<<<<< HEAD
-=======
 
         <Route path='/filter' element={<Filter/>}></Route> 
         <Route path='/lawyer-form' element={<LawyerForm/>}></Route>
-
->>>>>>> 7cf7dcb81bcbf33680f5e18699a9e33aa35f3c96
         </Routes>
         
       </div>
